99ffa6bf1019055a4dce011a78e01ceaa7e8dae2
RitobrataK <khanritobrata@gmail.com>
2025-02-03 05:39:38 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Disable GPU
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Suppress TensorFlow info and warning messages
 
 from flask import Flask, request, jsonify, send_from_directory
 import numpy as np
