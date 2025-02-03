0c8aebfd7d8841538b5a18f80f3c58e5114b8922
RitobrataK <khanritobrata@gmail.com>
2025-02-03 15:37:06 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -855,5 +855,5 @@ def test():
     return "Backend is running!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render dynamically assigns a port
+    port = int(os.environ.get("PORT", 10000))  # Render dynamically assigns a port
     app.run(host="0.0.0.0", port=port)
