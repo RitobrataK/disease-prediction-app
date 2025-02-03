3b4eaecb5adf1bf36f166101a6066c4b20b73733
RitobrataK <khanritobrata@gmail.com>
2025-02-03 13:48:16 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,7 @@ from flask_cors import CORS
 
 # Initialize Flask app
 app = Flask(__name__, static_folder="../frontend/dist")  # Serve React build files
-CORS(app)  # Allow CORS for specific frontend origins
-
-
+CORS(app, origins=["https://disease-prediction-app-5h44.onrender.com"]) # Allow CORS for specific frontend origins
 
 # Load the trained model and preprocessors
 try:
